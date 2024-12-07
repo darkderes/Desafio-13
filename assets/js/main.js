cc81c34b79ec87579cb316b57f3e31b320c685d0
Jorge Darderes <jdarderes@gmail.com>
2024-12-07 00:03:02 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+//funciond e expresion
+
+    const example = function(a, b, c) {
+        return a + b + c;
+    };
+
+    console.log(example(1, 2, 3)); // 6
+
+//funcion flecha
+    
+        const suma = (a, b) => a + b;
+    
+        console.log(suma(1, 2)); // 3
